4a29973366048bbbdf208e186345c0ec5154b67a
yamato0211 <kumayama0211@gmail.com>
2023-09-24 14:43:56 +0900

    add: todo server entrypoint

---
+++ todo/cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"go-micro-sample/todo/pkg/injection"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	adapter "go-micro-sample/todo/pkg/adapter/grpc"
+	todopb "go-micro-sample/todo/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	port := 8000
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+
+	usecase := injection.InitializeTodoUsecase()
+	us := adapter.NewTodoServer(usecase)
+
+	todopb.RegisterTodoServiceServer(s, us)
+
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
